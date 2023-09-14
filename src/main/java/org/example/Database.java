3e4a30a7104796d94816af659b598c7e038a0da1
Andrea Zora <zora.andrea93@gmail.com>
2023-09-14 14:28:54 +0200

    Esercizio 4 mySql

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.sql.*;
+
+
+//use JDBC for executing a query on the jdbc:mysql://localhost:3306/newdb database that:
+//adds a new string column of 30 chars called country to the students table
+//populate the new column with Italy for 2 students and Germany for the other 2 students
+public class Database {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Develhope",
+                    "root", ",$rZ8JgBeUbR#cw");
+
+            Statement statement = connection.createStatement();
+
+            statement.execute("ALTER TABLE students ADD COLUMN country VARCHAR(30)");
+            Statement statementItaly = connection.createStatement();
+            statementItaly.execute(
+                    "UPDATE students SET country= \"Italy\" WHERE student_id <=2");
+            Statement statementGermany = connection.createStatement();
+            statementGermany.execute(
+                    "UPDATE students SET country= \"Germany\" WHERE student_id<=4 AND student_id>2");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Colonna creata con successo!");
+    }
+}
